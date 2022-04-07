aa88419016457a3e7a187ec3d2161fe177f851cf
Chee Kian Teoh <cheekianteoh0306@gmail.com>
2022-04-07 18:44:39 -0400

    just add the navbar template
    add the navbar template, just like what we have in figma, but still couldn't show it in the main.html file

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# static files directory
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+
+]
+
