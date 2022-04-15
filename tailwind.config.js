6affc5026f311c2df39907d32b1a7c57c189a62c
Juan Carlos <46619361+juancwu@users.noreply.github.com>
2022-04-15 14:14:15 -0400

    Reconfigured tailwincss
    As per requested, I have reconfigured tailwindcss so that it does not generates a
    static folder inside the app folder and now it is generates in the root static/css folder.
    
    These changes have a minor impact on how we need to start the development environment.

---
+++ tailwind.config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  /**
+   * Look for class within all html files in our template folder
+   */
+  content: ['./templates/**/*.html'],
+
+  /**
+   * Theme configuration
+   */
+  theme: {
+    fontFamily: {
+        sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'sans-serif']
+    },
+
+    /**
+     * Extend the classes that tailwind has to offer instead of overriding them.
+     * Overriding any of the default category, such as margin, will get rid of all
+     * built-in classes for margin and leave the ones you have define.
+     */
+    extend: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'primary': 'var(--primary-color-bluishPurple)',
+            'secondary': 'var(--secondary-color-lightPurple)',
+            'rice-white': 'var(--rice-white)',
+            'dark': 'var(--dark-background)',
+            'success': 'var(--success)',
+            'error': 'var(--error)',
+            'darker-primary': 'var(--darker-primary)',
+            'inactive-primary': 'var(--inactive-primary-color)',
+            'inactive-secondary': 'var(--inactive-secondary-color)',
+            'inactive-dark': 'var(--inactive-dark-color)',
+            'outline-primary': 'var(--outline-primary-color)',
+            'outline-secondary': 'var(--outline-secondary-color)',
+            'outline-white': 'var(--outline-white-color)',
+            'outline-dark': 'var(--outline-dark-color)',
+            'outline-success': 'var(--outline-success-color)',
+            'outline-error': 'var(--outline-error-color)',
+            'white': "var(--white)",
+            'black': 'var(--black)',
+            'background': 'var(--background)'
+        },
+        maxWidth: {
+            'vw-full': '100vw',
+            'vh-full': '100vh'
+        },
+        backgroundImage: {
+            'primary-gradient': 'var(--primary-gradient-top-bottom)',
+            'dark-gradient': 'var(--dark-gradient-top-bottom)',
+        },
+        keyframes: {
+            'fade-in-upward': {
+                '0%': {
+                    opacity: '0',
+                    transform: 'translateY(10px)'
+                },
+                '100%': {
+                    opacity: '1',
+                    transform: 'translateY(0)'
+                }
+            }
+        },
+        animation: {
+            'fade-in-upward': 'fade-in-upward .5s ease-in'
+        }
+    },
+},
+plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+],
+}
