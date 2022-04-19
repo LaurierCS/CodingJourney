5ff65bb4f9c451197fa3266de18affedd30538f9
Juan Carlos <46619361+juancwu@users.noreply.github.com>
2022-04-19 12:43:57 -0400

    change landing page html name

--- app/views.py
+++ app/views.py
@@ -78,7 +78,7 @@ def register(request):
 
 
 def langing_page(request):
-    template = "app/landingpage.html"
+    template = "app/landing_page.html"
     context = {
         "site_title": "design your\ncoding journey",
         "site_description": "Coding Journey is a journal for programmers. Mark your current destination, create your coding path, explore other coders' journey and more!"
