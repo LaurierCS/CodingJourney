2b6c19e6a33ee8706d84ec4018be4ed11051ec15
Juan Carlos <46619361+juancwu@users.noreply.github.com>
2022-04-25 13:16:46 -0400

    added create/delete user profile signals

--- app/signals.py
+++ app/signals.py
@@ -5,29 +5,17 @@ from django.contrib.auth.models import User
 from .models import *
 
 # Example: Create a Profile for every new User
-def create_profile(sender, instance, created, **kwargs):
-    print('user created!!!')
-    # if created:
-    #     Profile.objects.create(user=instance)
-
-    return None
-
-
-@receiver(post_save, sender=Profile)
-def save_profile(sender, instance, created, **kwargs):
-    print('Profile saved!!!')
-    print('Sender: ', sender)
-    print('Instance', instance)
-    print('Created', created)
-    return None
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, kwargs):
+    if created:
+        user = instance
+        profile = Profile.objects.create(
+            user = user,
+        )
 
 @receiver(post_delete, sender=Profile)
-def del_profile(sender, instance, **kwargs):
-    print('Delete Profile...')
-
-
-# post_save.connect(save_profile, sender=Profile)
-# post_delete.connect(del_profile, sender=Profile)
-
+def del_profile(sender, instance, kwargs):
+    user = instance.user
+    user.delete()
 
 
