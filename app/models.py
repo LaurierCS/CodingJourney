b42f6ad255b1f1b490191a0e8f198d254a81afcf
ObiTracks <straightup.stack@gmail.com>
2022-05-03 18:53:17 -0400

    Important structural changes made to models and views. Needs to be worked a little more to work with Juan and Aleks version of the models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 from django.db.models.fields import *
 from django.template.defaultfilters import slugify
@@ -7,63 +6,64 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-  user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-  first_name = models.CharField(max_length=100, blank=False, default="John")
-  last_name = models.CharField(max_length=100, blank=False, default="Doe")
-  bio = models.TextField(max_length=500, blank=True)
-  image = models.ImageField(default="images/smiley.jpg", upload_to='images/', blank=True)
-  # 👇 THE "tech_roadmap" RELATIONSHIP BELOW IS THE TECHNOLOGIES THAT THE USER WANTS TO ACHIEVE 
-  # (if a tech is in this list, it will show up on the tree graph)
-  # This is all the tech they either want to work with or have already achieved experience with
-  # This will allow us to make the graph unique to each persons tech goals, instead of showing nodes for dozens of different
-  # technologies they aren't even aiming to achieve
-  tech_roadmap = models.ManyToManyField("Technology", blank=True, null=True) 
-  date_created = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, blank=False, default="John")
+    last_name = models.CharField(max_length=100, blank=False, default="Doe")
+    bio = models.TextField(max_length=500, blank=True)
+    image = models.ImageField(
+        default="images/smiley.jpg", upload_to='images/', blank=True)
+    # 👇 THE "tech_roadmap" RELATIONSHIP BELOW IS THE TECHNOLOGIES THAT THE USER WANTS TO ACHIEVE
+    # (if a tech is in this list, it will show up on the tree graph)
+    # This is all the tech they either want to work with or have already achieved experience with
+    # This will allow us to make the graph unique to each persons tech goals, instead of showing nodes for dozens of different
+    # technologies they aren't even aiming to achieve
+    tech_roadmap = models.ManyToManyField("Technology", blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-  def __str__(self):
-    return self.user.username
-
-
-class Language(models.Model):
-  name = models.CharField(max_length=100, blank=False)
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.user
 
 
-class Technology(models.Model):
-  #1. EVERY TECHNOLOGY IS EITHER A LANGUAGE, FRAMEWORK OR LIBRARY
-  #2. EVERY TECHNOLOGY CAN HAVE ANOTHER TECHNOLOGY AS A PARENT USING THE parent RELATIONSHIP VARIABLE
-  name = models.CharField(max_length=100, blank=False)
-  description = models.TextField(null=True, blank=True)
-  parents = models.ManyToManyField("self", null=False, blank=False)
-  TECHNOLOGY_TYPE = (
-        ('L','Language')
+class Skill(models.Model):
+    # 1. EVERY Skill IS EITHER A LANGUAGE, FRAMEWORK OR LIBRARY
+    # 2. EVERY Skill CAN HAVE ANOTHER Skill AS A PARENT USING THE parent RELATIONSHIP VARIABLE
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(null=True, blank=True)
+    parents = models.ManyToManyField("self", null=False, blank=False)
+    Skill_TYPE = (
+        ('L', 'Language'),
         ('F', 'Framework'),
         ('L', 'Library or Package'),
+        ('C', 'Programming Paradigm'),
     )
-  kind = models.CharField(max_length=40, choices=TECHNOLOGY_TYPE, default="L")
+    kind = models.CharField(max_length=40, choices=Skill_TYPE, default="L")
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
 
 class Experience(models.Model):
-  profile = models.ForeignKey("Profile", on_delete=models.CASCADE, null=False, blank=False)
-  technologies = models.ManyToManyField("Technology")
-  name = models.CharField(max_length=200)
-  EXPERIENCE_TYPE = (
+    profile = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, null=False, blank=False)
+    technologies = models.ManyToManyField("Technology")
+    name = models.CharField(max_length=200)
+    EXPERIENCE_TYPE = (
         ('E', 'Exploration'),
         ('P', 'Project'),
         ('L', 'Learning'),
         ('H', 'Hackathon'),
         ('E', 'Event'),
     )
-  kind = models.CharField(max_length=40, choices=EXPERIENCE_TYPE, default="E" )
-  description = models.TextField(null=True, blank=True)
-  likes_amount = models.IntegerField(default=0)
-  start_date = models.DateField(null=True, blank=True) #let these be allowed to be null for now until the widget is setup for date input sumbission
-  end_date = models.DateField(null=True, blank=True) #let these be allowed to be null for now until the widget is setup for date input sumbission
-  project_link = models.CharField(max_length=2000, null=True, blank=True)
-  image = models.ImageField(upload_to='images/', blank=True)
-  
-  def __str__(self):
-    return self.project_name
+    kind = models.CharField(
+        max_length=40, choices=EXPERIENCE_TYPE, default="E")
+    description = models.TextField(null=True, blank=True)
+    likes_amount = models.IntegerField(default=0)
+    # let these be allowed to be null for now until the widget is setup for date input sumbission
+    start_date = models.DateField(null=True, blank=True)
+    # let these be allowed to be null for now until the widget is setup for date input sumbission
+    end_date = models.DateField(null=True, blank=True)
+    project_link = models.CharField(max_length=2000, null=True, blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.name
