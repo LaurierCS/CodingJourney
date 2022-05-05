32a5ba17041121dad03546340f6719c11826c75f
AleksandarNe11 <59544247+AleksandarNe11@users.noreply.github.com>
2022-05-05 17:47:24 -0400

    Changes to models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from random import choices
 from django.db import models
 from django.db.models.fields import *
 from django.template.defaultfilters import slugify
@@ -17,18 +18,22 @@ class Profile(models.Model):
 
   
 class Experience(models.Model):
-  profile = models.ForeignKey("Profile", on_delete=models.CASCADE, null=True)
-  markers = models.ManyToManyField("Marker")
-  name = models.CharField(max_length=200)
   EXPERIENCE_TYPE = (
         ('E', 'Exploration'),
         ('P', 'Project'),
         ('L', 'Learning'),
         ('H', 'Hackathon'),
-        ('E', 'Event'),
-    )
+        ('Ev', 'Event'),
+  )
+
+  # Foreign Key Fields
+  profile = models.ForeignKey("Profile", on_delete=models.CASCADE, null=True)
+  skill = models.ForeignKey("Skill", on_delete=models.CASCADE, null=True)
+  # Text Fields 
+  name = models.CharField(max_length=200)
   kind = models.CharField(max_length=40, choices=EXPERIENCE_TYPE, default="E" )
   description = models.TextField(null=True, blank=True)
+  # Other Fields
   likes_amount = models.IntegerField(default=0)
   start_date = models.DateField(null=True, blank=True) #let these be allowed to be null for now until the widget is setup for date input sumbission
   end_date = models.DateField(null=True, blank=True) #let these be allowed to be null for now until the widget is setup for date input sumbission
@@ -44,12 +49,18 @@ class Experience(models.Model):
   Skill tree elements of type C will be parents to at least one node of type N 
 """
 class Skill(models.Model): 
+
+  node_type_choices = (
+    ("C", "Category"), ("N", "Node")
+  )
+  # Foreign key fields 
+  parent = models.ForeignKey("Skill", on_delete=models.CASCADE, null=True)
+
+  # Text fields
   id = models.CharField(primary_key=True, max_length=30)
-  children = models.ManyToOneRel("Skill", to='', field_name='')
   name = models.CharField(max_length=30)
   icon_HREF = models.URLField(max_length=200)
-  node_type_choices = [("C", "Category"), ("N", "Node")]
-  node_type = models.CharField(max_length=1, choices=node_type_choices)
+  node_type = models.CharField(max_length=40, choices=node_type_choices, default="C")
 
   def __str__(self): 
     return self.name
@@ -62,10 +73,21 @@ class Skill(models.Model):
   Each desired skill will have a set of experiences with it that can be nullable
 """
 class DesiredSkill(models.Model):
+  proficiency_choices = [
+    (0, "Aiming to Learn"),
+    (1, "Some Understanding"),
+    (2, "Some Proficiency"),
+    (3, "Capable"),
+    (4, "Able to Use Professionally"),
+    (5, "Expert"),
+    ]
+
+  # Foreign Key Fields
   user_id = models.ForeignKey("Profile", on_delete=models.CASCADE)
   skill = models.ForeignKey("Skill", on_delete=models.CASCADE)
   experiences = models.ManyToOneRel("Experience", to='', field_name='')
-  proficiency_choices = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]
+
+  # User Input Fields 
   proficiency = models.FloatField(choices=proficiency_choices, default=0)
   description = models.TextField(max_length=1000)
 
