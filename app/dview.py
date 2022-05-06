dadb81ee6ee37433a153a3c1dd4ea4ef9373ecdf
Juan Carlos Wu <46619361+juancwu@users.noreply.github.com>
2022-05-06 15:51:01 -0400

    added node images and labels

---
+++ app/dview.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+def d(r):
+    return render(r, 'app/d.html', {})
--- app/urls.py
+++ app/urls.py
@@ -27,3 +27,8 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+from . import dview
+urlpatterns.append(path("d/",dview.d,name='d'))
+
