a56087745f7fc5e663ef6a0032cb6dc5fe94a311
Juan Carlos Wu <46619361+juancwu@users.noreply.github.com>
2022-05-13 01:54:20 -0400

    added some todos

--- static/js/tree.js
+++ static/js/tree.js
@@ -41,6 +41,8 @@ const viewBox = {
   height: height * 1.5, // zoom out view
 };
 
+// FUNCTIONS
+
 function getSkills() {
   let returnList = []
   // console.log(tree_data)
@@ -88,117 +90,9 @@ function getLinkPath(d) {
 
 function onNodeClick(d) {
   // todo: implement on click behaviour
+  // todo: open node detail information sidebar
   console.log("Node click behaviour not implemented.");
 }
-
-const container = d3.select("#treeContainer");
-
-const treemap = d3
-  .tree()
-  .size([height, width])
-  .nodeSize([225, 300])
-  .separation((a, b) => {
-    if (
-      a.parent === b.parent &&
-      a.data.nodeType === NODE_TYPES.C &&
-      b.data.nodeType === NODE_TYPES.C
-    ) {
-      return 2;
-    }
-
-    return 1;
-  });
-const hierarchy = treemap(d3.stratify()(skills));
-const descendants = hierarchy.descendants()
-
-const svg = container
-  .append("svg")
-  .attr("id", "#tree")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("style", "cursor: move;")
-  .attr("viewBox", getViewBoxString(viewBox.x, viewBox.y, viewBox.width, viewBox.height)); // for zooming in/out
-
-const tree = svg.append("g").attr("data-tree", "true");
-
-// links are separated from the node group
-// it is easier to draw the links this way
-const links = tree
-  .selectAll(".link")
-  .data(hierarchy.links())
-  .enter()
-  .append("path")
-  .attr("class", "link")
-  .attr("d", (d) => getLinkPath(d))
-
-/**
- * Put all nodes into separate groups to handle on hover effects.
- * bug: fixed size category nodes will become a problem if the category name is too long.
- * todo: adjust category node size dynamically.
- */
-const nodes = tree
-  .selectAll(".node")
-  .data(descendants)
-  .enter()
-  .append("g")
-  .attr("class", "node-group")
-
-const rects = nodes
-  .append("rect")
-  .attr("class", (d) =>
-  d.data.nodeType === NODE_TYPES.C ? "node-category" : "node"
-  )
-  .attr("width", (d) => NODE_DIM[d.data.nodeType][0])
-  .attr("height", (d) => NODE_DIM[d.data.nodeType][1])
-  .attr("rx", (d) => (d.data.nodeType === NODE_TYPES.U ? "75" : "15"))
-  .attr("ry", (d) => (d.data.nodeType === NODE_TYPES.U ? "75" : "15"))
-  .attr("x", (d) => d.data.nodeType === NODE_TYPES.C ? d.x - 50 : d.x )
-  .attr("y", (d) => d.y)
-  .attr("data-node-id", d => d.id)
-  .on("click", onNodeClick);
-
-const nodeLabel = nodes
-  .append("text")
-  .text(d => d.data.name)
-  .attr("class", d => d.data.nodeType !== NODE_TYPES.C ? "node-label" : "node-label node-title")
-  .attr("x", d => {
-    // because the x in d.x does not reflect the x position of the rectangle
-    // to center the text for a category node, we need to get the x from the rect element directly.
-    let x;
-    if (d.data.nodeType === NODE_TYPES.C) {
-      let r = document.querySelector(`rect[data-node-id="${d.id}"`)
-      x = parseFloat(r.getAttribute("x"))
-    } else {
-      x = d.x
-    }
-    return x + NODE_DIM[d.data.nodeType][0] / 2;
-  })
-  .attr("y", d => d.data.nodeType === NODE_TYPES.C ? d.y + NODE_DIM.C[1] / 2 : d.y + NODE_DIM[d.data.nodeType][1] + 20)
-  .attr("text-anchor", "middle")
-  .attr("dominant-baseline", "middle")
-  .attr("data-node-label-id", d => d.id)
-
-const nodeImage = nodes
-  .append("image")
-  .attr("href", d => {
-    // todo: return iconHref instead
-    return "/static/images/python_logo.png"
-  })
-  .attr("class", "node-image")
-  .attr("width", d => d.data.nodeType === NODE_TYPES.N ? NODE_IMG_SIZE : 0) // todo: return actual size for user nodes
-  .attr("height", d => d.data.nodeType === NODE_TYPES.N ? NODE_IMG_SIZE : 0) // todo: return actual szie for user nodes
-  .attr("x", d => d.x + NODE_DIM[d.data.nodeType][0] / 2 - NODE_IMG_SIZE / 2)
-  .attr("y", d => d.y + NODE_DIM[d.data.nodeType][1] / 2 - NODE_IMG_SIZE / 2)
-  .attr("data-node-image-id", d => d.id)
-
-// update viewbox
-console.log(document.querySelector('g[data-tree="true"]').getClientRects())
-
-// add panning
-const originPoint = { x: -1, y: -1 };
-const newViewBox = { ...viewBox };
-const svgEl = document.getElementById("#tree");
-let isPointerDown = false; 
 
 function getViewBoxString(x, y, w, h) {
   return `${x} ${y} ${w} ${h}`;
@@ -274,6 +168,116 @@ function onZoom(e) {
     getViewBoxString(viewBox.x, viewBox.y, viewBox.width, viewBox.height)
   );
 }
+// END OF FUNCTIONS
+
+// START BUILDING TREE
+const container = d3.select("#treeContainer");
+
+const treemap = d3
+  .tree()
+  .size([height, width])
+  .nodeSize([225, 300])
+  .separation((a, b) => {
+    // todo: determine the space between category nodes based on the lenght of the labels.
+    if (
+      a.parent === b.parent &&
+      a.data.nodeType === NODE_TYPES.C &&
+      b.data.nodeType === NODE_TYPES.C
+    ) {
+      return 2;
+    }
+
+    return 1;
+  });
+const hierarchy = treemap(d3.stratify()(skills));
+const descendants = hierarchy.descendants()
+
+const svg = container
+  .append("svg")
+  .attr("id", "#tree")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("style", "cursor: move;")
+  .attr("viewBox", getViewBoxString(viewBox.x, viewBox.y, viewBox.width, viewBox.height)); // for zooming in/out
+
+const tree = svg.append("g").attr("data-tree", "true");
+
+// links are separated from the node group
+// it is easier to draw the links this way
+const links = tree
+  .selectAll(".link")
+  .data(hierarchy.links())
+  .enter()
+  .append("path")
+  .attr("class", "link")
+  .attr("d", (d) => getLinkPath(d))
+
+/**
+ * Put all nodes into separate groups to handle on hover effects.
+ * bug: fixed size category nodes will become a problem if the category name is too long.
+ * bug: the text will overflow, the node wont expand.
+ * todo: adjust category node size dynamically.
+ */
+const nodes = tree
+  .selectAll(".node")
+  .data(descendants)
+  .enter()
+  .append("g")
+  .attr("class", "node-group")
+
+const rects = nodes
+  .append("rect")
+  .attr("class", (d) =>
+  d.data.nodeType === NODE_TYPES.C ? "node-category" : "node"
+  )
+  .attr("width", (d) => NODE_DIM[d.data.nodeType][0])
+  .attr("height", (d) => NODE_DIM[d.data.nodeType][1])
+  .attr("rx", (d) => (d.data.nodeType === NODE_TYPES.U ? "75" : "15"))
+  .attr("ry", (d) => (d.data.nodeType === NODE_TYPES.U ? "75" : "15"))
+  .attr("x", (d) => d.data.nodeType === NODE_TYPES.C ? d.x - 50 : d.x )
+  .attr("y", (d) => d.y)
+  .attr("data-node-id", d => d.id)
+  .on("click", onNodeClick);
+
+const nodeLabel = nodes
+  .append("text")
+  .text(d => d.data.name)
+  .attr("class", d => d.data.nodeType !== NODE_TYPES.C ? "node-label" : "node-label node-title")
+  .attr("x", d => {
+    // because the x in d.x does not reflect the x position of the rectangle
+    // to center the text for a category node, we need to get the x from the rect element directly.
+    let x;
+    if (d.data.nodeType === NODE_TYPES.C) {
+      let r = document.querySelector(`rect[data-node-id="${d.id}"`)
+      x = parseFloat(r.getAttribute("x"))
+    } else {
+      x = d.x
+    }
+    return x + NODE_DIM[d.data.nodeType][0] / 2;
+  })
+  .attr("y", d => d.data.nodeType === NODE_TYPES.C ? d.y + NODE_DIM.C[1] / 2 : d.y + NODE_DIM[d.data.nodeType][1] + 20)
+  .attr("text-anchor", "middle")
+  .attr("dominant-baseline", "middle")
+  .attr("data-node-label-id", d => d.id)
+
+const nodeImage = nodes
+  .append("image")
+  .attr("href", d => {
+    // todo: return iconHref instead
+    return "/static/images/python_logo.png"
+  })
+  .attr("class", "node-image")
+  .attr("width", d => d.data.nodeType === NODE_TYPES.N ? NODE_IMG_SIZE : 0) // todo: return actual size for user nodes
+  .attr("height", d => d.data.nodeType === NODE_TYPES.N ? NODE_IMG_SIZE : 0) // todo: return actual szie for user nodes
+  .attr("x", d => d.x + NODE_DIM[d.data.nodeType][0] / 2 - NODE_IMG_SIZE / 2)
+  .attr("y", d => d.y + NODE_DIM[d.data.nodeType][1] / 2 - NODE_IMG_SIZE / 2)
+  .attr("data-node-image-id", d => d.id)
+
+// add panning
+const originPoint = { x: -1, y: -1 };
+const newViewBox = { ...viewBox };
+const svgEl = document.getElementById("#tree");
+let isPointerDown = false; 
 
 if (window.PointerEvent) {
   svgEl.addEventListener("pointerdown", onPointerDown);
@@ -293,4 +297,5 @@ if (window.PointerEvent) {
   svgEl.addEventListener("touchmove", onPointerMove);
 }
 
+// add zoom in/out
 svgEl.addEventListener("wheel", onZoom);
