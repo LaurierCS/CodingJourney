e9fd2ddd749f84e433502a6289a5f57a44fa1883
Juan Carlos Wu <46619361+juancwu@users.noreply.github.com>
2022-05-25 22:18:04 -0400

    add whitenoise middleware in settings

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -51,12 +51,14 @@ INTERNAL_IPS = ['127.0.0.1']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # serve static files on heroku
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'Webapp.urls'
