faa3fcd5b8ce48ffd2a634d62aa37a5a23764e9b
ObiTracks <straightup.stack@gmail.com>
2022-05-25 20:24:11 -0400

    Setup for launch, logged into heroku cli, installed requirments (gunicorn & whitenoise), added postegres database

--- Webapp/settings.py
+++ Webapp/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'Webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd2k8lv1pnc0g6g',
+        'HOST': 'ec2-54-211-255-161.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'pohqcaesblokciw',
+        'PASSWORD': '8984b048524771ebf59dd23ea627e2bd8a7d5f7fe2ca6131b3e613154a29c1bf',
     }
 }
 
